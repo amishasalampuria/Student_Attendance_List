4ee96197f9a65a56aa3d00fd1c61363ced9419e0
amisha <salampuriaamisha@gmail.com>
2024-07-18 12:55:32 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,6 @@ const App = () => {
   return (
 
     <div>
-      {/* <UserData /> */}
       <Routes>
         <Route path='/' element={<Users />} />
         <Route path="/create" element={<CreateStudent />} />
